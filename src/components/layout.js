9abd3254fa4f9eef7a36d598e6c73ccc911c3699
jacquesdebar <jacques.debar@gmail.com>
2020-05-17 20:01:00 -0700

    remove default language

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,12 +33,8 @@ const Layout = ({ children }) => {
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <main style={{ minHeight: `70vh`}}>{children}</main>
+        <hr style={{marginTop: `40px`}}/>
       </div>
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>header</h1>
+    <p>Check back in a few days for more information.</p>
+    {/*
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    </div> */ }
+    <Link to="/page-2/">Rememberance board</Link>
   </Layout>
 )
 
